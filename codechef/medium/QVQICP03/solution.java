cdf05287cf0da04978206d3fb174973287854253
Sameer Raj <sr5846586@gmail.com>
2026-09-09 19:34:58 +0530

    ✅ QVQICP03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/QVQICP03/solution.java
+++ codechef/medium/QVQICP03/solution.java
@@ -1,6 +1,6 @@
 class Codechef {
     public static void main(String[] args) {
 	    // Printing a good morning greeting message
-        System.out.println("Good morning!") ;
+        System.out.println("Good morning!");  
     }
 }
